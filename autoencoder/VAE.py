377525956f8f5e31e0f228666941012521fb7da6
Gemma <gwhite11@student.bbk.ac.uk>
2023-06-23 22:47:54 +0100

    2

--- autoencoder/VAE.py
+++ autoencoder/VAE.py
@@ -9,6 +9,37 @@ random_seed = 42
 random.seed(random_seed)
 torch.manual_seed(random_seed)
 
+# Split the data into training, validation, and test sets
+train_data = []
+val_data = []
+test_data = []
+
+train_ratio = 0.8
+val_ratio = 0.1
+test_ratio = 0.1
+batch_size = 32
+
+input_files_directory = 'C://Users//gemma//PycharmProjects//pythonProject1//autoencoder//pdb_files//graph_data'
+data_list = []
+dataloader = DataLoader(data_list, batch_size=batch_size)
+for filename in os.listdir(input_files_directory):
+    if filename.endswith(".pt"):
+        file_path = os.path.join(input_files_directory, filename)
+        data = torch.load(file_path)
+        data_list.append(data)
+
+num_examples = len(data_list)
+train_cutoff = int(train_ratio * num_examples)
+val_cutoff = train_cutoff + int(val_ratio * num_examples)
+
+train_data = data_list[:train_cutoff]
+val_data = data_list[train_cutoff:val_cutoff]
+test_data = data_list[val_cutoff:]
+
+train_loader = DataLoader(train_data, batch_size=batch_size, shuffle=True)
+val_loader = DataLoader(val_data, batch_size=batch_size)
+test_loader = DataLoader(test_data, batch_size=batch_size)
+
 
 class VAE(nn.Module):
     def __init__(self, input_dim, hidden_dim, latent_dim, num_levels, coarse_grain_dims, dropout_rate=0.5):
@@ -83,37 +114,21 @@ hidden_dim = 128
 latent_dim = 32  # Set the desired dimensionality of the latent space
 num_levels = 3
 coarse_grain_dims = [3, 3, 3]
-batch_size = 32
 dropout_rate = 0.5
 num_epochs = 100
 learning_rate = 0.001
-num_folds = 5
-translate_range = 0.1  # Adjust the translation range as per your requirements
 
-input_files_directory = 'C://Users//gemma//PycharmProjects//pythonProject1//autoencoder//pdb_files//graph_data'
-data_list = []
-for filename in os.listdir(input_files_directory):
-    if filename.endswith(".pt"):
-        file_path = os.path.join(input_files_directory, filename)
-        data = torch.load(file_path)
-        data_list.append(data)
+# Create three VAE models for different levels of coarse-graining
+model_coarse = VAE(input_dim, hidden_dim, latent_dim, 1, [coarse_grain_dims[0]], dropout_rate)
+model_coarser = VAE(input_dim, hidden_dim, latent_dim, 2, coarse_grain_dims[:2], dropout_rate)
+model_coarsest = VAE(input_dim, hidden_dim, latent_dim, 3, coarse_grain_dims, dropout_rate)
 
-train_data = []
-val_data = []
-for i, data in enumerate(data_list):
-    if i % num_folds == 0:
-        val_data.append(data)
-    else:
-        train_data.append(data)
-
-train_loader = DataLoader(train_data, batch_size=batch_size, shuffle=True)
-val_loader = DataLoader(val_data, batch_size=batch_size)
-
-model = VAE(input_dim, hidden_dim, latent_dim, num_levels, coarse_grain_dims, dropout_rate)
+models = [model_coarse, model_coarser, model_coarsest]
 
 reconstruction_loss_function = nn.MSELoss()
 
-optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate)
+optimizers = [torch.optim.Adam(model.parameters(), lr=learning_rate) for model in models]
+
 sample_data = data_list[0]  # Choose a sample data element from data_list
 print(f"x shape: {sample_data.x.shape}")
 print(f"edge_index shape: {sample_data.edge_index.shape}")
@@ -126,74 +141,77 @@ def loss_function(recon_x, x, mu, logvar):
 
 
 for epoch in range(num_epochs):
-    model.train()
-    total_loss = 0.0
+    for model, optimizer in zip(models, optimizers):
+        model.train()
+        total_loss = 0.0
 
-    for batch in train_loader:
-        x = batch.x
-        edge_index = batch.edge_index
-
-        outputs, mu, logvar = model(x, edge_index)
-        reconstruction_loss = loss_function(outputs[-1], x, mu, logvar)
-
-        optimizer.zero_grad()
-        reconstruction_loss.backward()
-        optimizer.step()
-
-        total_loss += reconstruction_loss.item() * x.size(0)
-
-    average_loss = total_loss / len(train_loader.dataset)
-    print(f"Epoch: {epoch + 1}/{num_epochs}, Average Loss: {average_loss}")
-
-    model.eval()
-    with torch.no_grad():
-        total_val_loss = 0.0
-        for batch in val_loader:
+        for batch in train_loader:
             x = batch.x
             edge_index = batch.edge_index
 
             outputs, mu, logvar = model(x, edge_index)
-            val_loss = loss_function(outputs[-1], x, mu, logvar)
+            reconstruction_loss = loss_function(outputs[-1], x, mu, logvar)
 
-            total_val_loss += val_loss.item() * x.size(0)
+            optimizer.zero_grad()
+            reconstruction_loss.backward()
+            optimizer.step()
 
-        avg_val_loss = total_val_loss / len(val_loader.dataset)
+            total_loss += reconstruction_loss.item() * x.size(0)
 
-    print(f"Validation Loss: {avg_val_loss}")
+        average_loss = total_loss / len(train_loader.dataset)
+        print(f"Epoch: {epoch + 1}/{num_epochs}, Average Loss: {average_loss}")
 
-dataloader = DataLoader(data_list, batch_size=batch_size)  # Create a DataLoader for all data
+        model.eval()
+        with torch.no_grad():
+            total_val_loss = 0.0
+            for batch in val_loader:
+                x = batch.x
+                edge_index = batch.edge_index
 
-# Save the trained model
-torch.save(model.state_dict(), 'best_model.pt')
+                outputs, mu, logvar = model(x, edge_index)
+                val_loss = loss_function(outputs[-1], x, mu, logvar)
 
-# Load the trained model
-model = VAE(input_dim, hidden_dim, latent_dim, num_levels, coarse_grain_dims, dropout_rate)
-model.load_state_dict(torch.load('best_model.pt'))
-model.eval()  # Set the model to evaluation mode
+                total_val_loss += val_loss.item() * x.size(0)
 
-# Load the new protein data
-new_data = torch.load('new_protein.pt')
+            avg_val_loss = total_val_loss / len(val_loader.dataset)
 
-# Apply the model to the new data
-new_data_loader = DataLoader([new_data], batch_size=1)
+        print(f"Validation Loss: {avg_val_loss}")
 
-new_coarse_grained_reps = []
-for data in new_data_loader:
-    x, edge_index, batch = data.x, data.edge_index, data.batch
-    outputs, mu, logvar = model(x, edge_index)
-    last_level_output = outputs[-1]  # Get the output of the last level in the hierarchy
+# Define the testing function
+def test():
+    # not sure if I need a test here since there is not a 'ground truth' to test the CG reps against
+    pass
 
-    if last_level_output.numel() == 0:
-        # Skip pooling if the tensor or batch is empty
-        continue
+# Call the test function after training
+test()
 
-    # Create a new batch tensor with correct size
-    batch_size = last_level_output.size(0)
-    new_batch = torch.arange(batch_size).to(x.device)
+# Generate CG reps for each model
+coarse_grained_reps = []
+for model in models:
+    model.eval()
+    with torch.no_grad():
+        current_coarse_grained_rep = []
 
-    # Perform pooling if the tensor is not empty
-    rep = global_mean_pool(last_level_output, new_batch)
-    new_coarse_grained_reps.append(rep)
+        for batch in dataloader:
+            x = batch.x
+            edge_index = batch.edge_index
 
-# Save the coarse-grained representations
-torch.save(new_coarse_grained_reps, 'new_protein_coarse_grained_reps.pt')
+            outputs, _, _ = model(x, edge_index)
+            last_level_output = outputs[-1]
+
+            if last_level_output.numel() == 0:
+                continue
+
+            batch_size = last_level_output.size(0)
+            new_batch = torch.arange(batch_size).to(x.device)
+
+            rep = global_mean_pool(last_level_output, new_batch)
+            current_coarse_grained_rep.append(rep)
+
+        coarse_grained_reps.append(current_coarse_grained_rep)
+
+# Save the CG reps for each level
+for level, reps in enumerate(coarse_grained_reps):
+    torch.save(reps, f'coarse_grained_reps_level_{level+1}.pt')
+
+
