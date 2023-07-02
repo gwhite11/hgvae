31dd7535117b71ff381e88de78184649e62389c6
Gemma <gwhite11@student.bbk.ac.uk>
2023-07-02 14:15:07 +0100

    the autoencoder runs - trying something different with beta (no idea if this disentanglement thing will be a good or bad thing

--- autoencoder/bvae.py
+++ autoencoder/bvae.py
@@ -1,138 +1,128 @@
-import os
-import random
-import numpy as np
 import torch
 import torch.nn as nn
+import random
+import os
+from torch_geometric.nn import GCNConv, TopKPooling, GATConv, global_mean_pool
 from torch_geometric.data import DataLoader, Data
-from torch_geometric.nn import GCNConv, TopKPooling, GATConv
 from sklearn.decomposition import PCA
+import numpy as np
 import pytorch_lightning as pl
-from torch.distributions import Normal
+
+random_seed = 42
+random.seed(random_seed)
+torch.manual_seed(random_seed)
+torch.cuda.manual_seed_all(random_seed)
 
 
 class HierarchicalCoarseGraining(pl.LightningModule):
-    def __init__(self, input_dim, latent_dim, num_levels, coarse_grain_dims, dropout_rate, beta=1.0):
-
+    def __init__(self, input_dim, latent_dim, num_levels, coarse_grain_dims, dropout_rate=0.5):
         super(HierarchicalCoarseGraining, self).__init__()
 
-        # add beta as a class attribute
-        self.beta = beta
-
-        self.latent_dim = latent_dim
         self.encoders = nn.ModuleList()
         self.decoders = nn.ModuleList()
         self.pooling = nn.ModuleList()
         self.dropout = nn.ModuleList()
-        self.batch_norms = nn.ModuleList()
+
+        # new addition for B-VAE
+        self.fc_mu = nn.Linear(latent_dim, latent_dim)
+        self.fc_var = nn.Linear(latent_dim, latent_dim)
 
         for level in range(num_levels):
             input_dim_level = input_dim if level == 0 else coarse_grain_dims[level - 1]
             output_dim_level = coarse_grain_dims[level]
 
             if level == 0:
-                encoder = GCNConv(input_dim_level, latent_dim*2) # the encoder output size is doubled to account for mu and logvar
+                encoder = GCNConv(input_dim_level, latent_dim)
             else:
-                encoder = GATConv(input_dim_level, latent_dim*2) # the encoder output size is doubled to account for mu and logvar
+                encoder = GATConv(input_dim_level, latent_dim)
 
             self.encoders.append(encoder)
 
-            decoder = GCNConv(latent_dim, output_dim_level) # The decoder input is latent_dim now
+            decoder = GCNConv(latent_dim, output_dim_level)
             self.decoders.append(decoder)
 
             dropout = nn.Dropout(p=dropout_rate)
             self.dropout.append(dropout)
 
-            batch_norm = nn.BatchNorm1d(latent_dim*2)
-            self.batch_norms.append(batch_norm)
-
             pool = TopKPooling(latent_dim)
             self.pooling.append(pool)
+
+    def reparameterize(self, mu, logvar):
+        std = torch.exp(logvar / 2)
+        eps = torch.randn_like(std)
+        return mu + eps * std
 
     def forward(self, x, edge_index):
         outputs = []
         batch_indices = []
-        kl_divergence = 0.0
 
-        for encoder, decoder, dropout, batch_norm, pool in zip(
-            self.encoders, self.decoders, self.dropout, self.batch_norms, self.pooling
-        ):
+        for encoder, decoder, dropout, pool in zip(self.encoders, self.decoders, self.dropout, self.pooling):
             x = dropout(x)
-            x = batch_norm(encoder(x, edge_index))
-            mu, logvar = x.chunk(2, dim=-1) # splitting the tensor along the last dimension
-            q_z = Normal(mu, logvar.mul(0.5).exp())
-
-            z = q_z.rsample()
-            kl_divergence += self.kl_divergence(mu, logvar)
-
-            x, edge_index, _, batch, _, _ = pool(z, edge_index) # use the sampled z for pooling
+            z = encoder(x, edge_index)
+            mu = self.fc_mu(z)
+            log_var = self.fc_var(z)
+            z = self.reparameterize(mu, log_var)
+            x, edge_index, _, batch, _, _ = pool(z, edge_index)
             x = decoder(x, edge_index)
 
             outputs.append(x)
             batch_indices.append(batch)
 
-        return outputs, batch_indices, kl_divergence
+        return outputs, batch_indices, mu, log_var
 
-    def kl_divergence(self, mu, logvar):
-        return -0.5 * torch.sum(1 + logvar - mu.pow(2) - logvar.exp(), dim=-1)
-
-
-    def compute_loss(self, batch):
-        x = batch.x
-        edge_index = batch.edge_index
-        outputs, _, kl_divergence = self(x, edge_index)
-
-        reconstruction_loss = sum(
-            self.reconstruction_loss_function(level_output, x[:level_output.size(0)])
-            for level_output in outputs
-        )
-
-        # multiply KL divergence with beta
-        return reconstruction_loss + self.beta * kl_divergence
+    def loss_function(self, outputs, x, mu, log_var):
+        reconstruction_loss = 0.0
+        for level_output in outputs:
+            reconstruction_loss += self.reconstruction_loss_function(level_output, x[:level_output.size(0)])
+        KLD = -0.5 * torch.sum(1 + log_var - mu.pow(2) - log_var.exp())
+        return reconstruction_loss + KLD
 
     def training_step(self, batch, batch_idx):
-        loss = self.compute_loss(batch)
-        self.log("train_loss", loss, prog_bar=True, logger=True, on_step=True, on_epoch=True)
+        x = batch.x
+        edge_index = batch.edge_index
+
+        outputs, _, mu, log_var = self(x, edge_index)
+
+        loss = self.loss_function(outputs, x, mu, log_var)
+
+        self.log("train_loss", loss, prog_bar=True, logger=True, batch_size=batch.x.size(0))
         return loss
 
     def validation_step(self, batch, batch_idx):
-        loss = self.compute_loss(batch)
-        self.log("val_loss", loss, prog_bar=True, logger=True, on_step=True, on_epoch=True)
-        return loss
+        x = batch.x
+        edge_index = batch.edge_index
 
+        outputs, _, mu, log_var = self(x, edge_index)
+
+        loss = self.loss_function(outputs, x, mu, log_var)
+
+        self.log("val_loss", loss, prog_bar=True, logger=True, batch_size=batch.x.size(0))
     def configure_optimizers(self):
-        return torch.optim.Adam(self.parameters(), lr=self.learning_rate, weight_decay=self.weight_decay)
+        return torch.optim.Adam(self.parameters(), lr=self.learning_rate)
+
+    def reconstruction_loss_function(self, recon_x, x):
+        MSE = nn.MSELoss()
+        return MSE(recon_x, x)
 
 
 def estimate_latent_dim(data_list, variance_threshold=0.95):
-    """Estimates the latent dimension for the data."""
     node_features = torch.cat([data.x for data in data_list], dim=0).detach().numpy()
     pca = PCA()
     pca.fit(node_features)
     cumulative_explained_variance = np.cumsum(pca.explained_variance_ratio_)
     latent_dim = np.argmax(cumulative_explained_variance > variance_threshold)
-    return max(latent_dim, 1)  # Ensure the latent dimension is at least 1
+    return latent_dim
 
 
-def load_data(data_dir):
-    """Loads .pt data files from a directory."""
-    data_list = []
+def random_translate(data, translate_range):
+    translation = torch.FloatTensor(data.pos.size()).uniform_(-translate_range, translate_range)
+    data.pos += translation
+    return data
 
-    for filename in os.listdir(data_dir):
-        if filename.endswith(".pt"):
-            file_path = os.path.join(data_dir, filename)
-            data = torch.load(file_path)
-            data_list.append(data)
-
-    return data_list
-
-
-random_seed = 42
-random.seed(random_seed)
-if torch.cuda.is_available():
-    torch.manual_seed(random_seed)
 
 input_dim = 3
 variance_threshold = 0.95
+hidden_dim = 128
 num_levels = 3
 coarse_grain_dims = [3, 3, 3]
 batch_size = 32
@@ -140,45 +130,46 @@ dropout_rate = 0.5
 num_epochs = 1000
 learning_rate = 0.001
 num_folds = 5
+translate_range = 0.1
 
 input_files_directory = 'C://Users//gemma//PycharmProjects//pythonProject1//autoencoder//pdb_files//graph_data'
-
-try:
-    data_list = load_data(input_files_directory)
-except Exception as e:
-    print(f"Error: {e}")
-    exit()
+data_list = []
+for filename in os.listdir(input_files_directory):
+    if filename.endswith(".pt"):
+        file_path = os.path.join(input_files_directory, filename)
+        data = torch.load(file_path)
+        data_list.append(data)
 
 latent_dim = estimate_latent_dim(data_list, variance_threshold)
 
-train_data = [data for i, data in enumerate(data_list) if i % num_folds != 0]
-val_data = [data for i, data in enumerate(data_list) if i % num_folds == 0]
+train_data = []
+val_data = []
+for i, data in enumerate(data_list):
+    if i % num_folds == 0:
+        val_data.append(data)
+    else:
+        train_data.append(data)
 
 train_loader = DataLoader(train_data, batch_size=batch_size, shuffle=True)
 val_loader = DataLoader(val_data, batch_size=batch_size)
 
-model = HierarchicalCoarseGraining(input_dim, latent_dim, num_levels, coarse_grain_dims, dropout_rate, beta=5.0)
-
-
-model.reconstruction_loss_function = nn.MSELoss()
+model = HierarchicalCoarseGraining(input_dim, latent_dim, num_levels, coarse_grain_dims, dropout_rate)
 model.learning_rate = learning_rate
-model.weight_decay = 1e-5
+
 
 trainer = pl.Trainer(max_epochs=num_epochs)
 trainer.fit(model, train_loader, val_loader)
 
 # Save the trained model
-model_path = 'best_model.pt'
-torch.save(model.state_dict(), model_path)
+torch.save(model.state_dict(), 'best_model.pt')
 
 # Load the trained model
 model = HierarchicalCoarseGraining(input_dim, latent_dim, num_levels, coarse_grain_dims, dropout_rate)
-model.load_state_dict(torch.load(model_path))
+model.load_state_dict(torch.load('best_model.pt'))
 model.eval()
 
-# Load new protein data
-new_data_path = 'C://Users//gemma//PycharmProjects//pythonProject1//autoencoder//pdb_files//chi_graph'
-new_data = torch.load(new_data_path)
+# Load the new protein data
+new_data = torch.load('C://Users//gemma//PycharmProjects//pythonProject1//autoencoder//pdb_files//chi_graph')
 
 # Apply the model to the new data
 new_data_loader = DataLoader([new_data], batch_size=1)
@@ -188,18 +179,21 @@ new_reconstructed_reps = []
 
 for data in new_data_loader:
     x, edge_index, batch = data.x, data.edge_index, data.batch
-    outputs, batch_indices, _ = model(x, edge_index) # unpack all three returned values
+    outputs, batch_indices, _, _ = model(x, edge_index)
 
     # Coarse-grained representations
-    coarse_grained_reps = [
-        Data(x=level_output, edge_index=edge_index, batch=batch_index)
-        for level_output, batch_index in zip(outputs, batch_indices)
-    ]
+    coarse_grained_reps = []
+    for level_output, batch_index in zip(outputs, batch_indices):
+        graph = Data(x=level_output, edge_index=edge_index, batch=batch_index)
+        coarse_grained_reps.append(graph)
     new_coarse_grained_reps.append(coarse_grained_reps)
 
     # Reconstructed representations
-    new_reconstructed_reps.append(model.decoders[-1](outputs[-1], edge_index))
+    reconstructed_reps = model.decoders[-1](outputs[-1], edge_index)
+    new_reconstructed_reps.append(reconstructed_reps)
 
-# Save the coarse-grained representations and reconstructed representations
+# Save the coarse-grained representations
 torch.save(new_coarse_grained_reps, 'new_protein_coarse_grained_reps.pt')
+
+# Save the reconstructed representations
 torch.save(new_reconstructed_reps, 'new_protein_reconstructed_reps.pt')
