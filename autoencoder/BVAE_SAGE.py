remaking the graphs - I originally made the tensor the wrong shape - ooops!

autoencoder/BVAE_SAGE.py
@@ -3,7 +3,7 @@ import torch.nn as nn
 import random
 import os
 from torch_geometric.nn import GraphConv, GCNConv, TopKPooling, GraphSAGE
-from torch_geometric.data import DataLoader, Data
+from torch_geometric.data import DataLoader
 from sklearn.decomposition import PCA
 from sklearn.cluster import AgglomerativeClustering
 import numpy as np
@@ -28,10 +28,7 @@ class VariationalAutoencoder(pl.LightningModule):
             input_dim_level = input_dim if level == 0 else coarse_grain_dims[level - 1]
             output_dim_level = coarse_grain_dims[level]
 
-            if level == 0:
-                encoder = GraphSAGE(input_dim_level, latent_dim, num_layers, aggr='mean')
-            else:
-                encoder = GraphConv(input_dim_level, latent_dim)
+            encoder = GraphSAGE(input_dim_level, latent_dim, num_layers, aggr='mean')
 
             self.encoder.append(encoder)
 
@@ -49,12 +46,14 @@ class VariationalAutoencoder(pl.LightningModule):
         self.fc_mu = nn.Linear(latent_dim, latent_dim)
         self.fc_logvar = nn.Linear(latent_dim, latent_dim)
 
+        self.learning_rate = None  # Define learning_rate attribute
+
     def encode(self, xs, edge_indices):
         for encoder, dropout, pool, x, edge_index in zip(self.encoder, self.dropout, self.pooling, xs, edge_indices):
             x = dropout(x)
-            x = encoder(x, edge_index)
+            print(edge_index.shape)
+            x = encoder(x, edge_index)  # Directly pass edge_index
             x, edge_index, _, batch, _, _ = pool(x, edge_index)
-
         x = x.mean(dim=0)  # Global pooling
         return self.fc_mu(x), self.fc_logvar(x)
 
@@ -69,9 +68,10 @@ class VariationalAutoencoder(pl.LightningModule):
         return z
 
     def forward(self, xs, edge_indices):
+        print(edge_indices.shape)
         mu, logvar = self.encode(xs, edge_indices)
         z = self.reparameterize(mu, logvar)
-        return self.decode(z, edge_indices[-1]), mu, logvar
+        return self.decode(z, edge_indices[-1]), mu, logvar  # Directly pass edge_index
 
     def training_step(self, batch, batch_idx):
         x = batch.x
@@ -80,12 +80,12 @@ class VariationalAutoencoder(pl.LightningModule):
         outputs, mu, logvar = self(x, edge_index)
         reconstruction_loss = 0.0
         for level_output in outputs:
-            reconstruction_loss += self.reconstruction_loss_function(level_output, x[:level_output.size(0)])
+            reconstruction_loss += nn.MSELoss()(level_output, x[:level_output.size(0)])
 
         kl_loss = -0.5 * torch.sum(1 + logvar - mu.pow(2) - logvar.exp())
         loss = reconstruction_loss + kl_loss
 
-        self.log("train_loss", loss, prog_bar=True, logger=True, batch_size=batch.x.size(0))
+        self.log("train_loss", loss, prog_bar=True, logger=True, on_step=True)  # Use on_step=True
         return loss
 
     def validation_step(self, batch, batch_idx):
@@ -95,47 +95,44 @@ class VariationalAutoencoder(pl.LightningModule):
         outputs, mu, logvar = self(x, edge_index)
         reconstruction_loss = 0.0
         for level_output in outputs:
-            reconstruction_loss += self.reconstruction_loss_function(level_output, x[:level_output.size(0)])
+            reconstruction_loss += nn.MSELoss()(level_output, x[:level_output.size(0)])
 
         kl_loss = -0.5 * torch.sum(1 + logvar - mu.pow(2) - logvar.exp())
         loss = reconstruction_loss + kl_loss
 
-        self.log("val_loss", loss, prog_bar=True, logger=True, batch_size=batch.x.size(0))
+        self.log("val_loss", loss, prog_bar=True, logger=True, on_step=True)  # Use on_step=True
         return loss
 
     def configure_optimizers(self):
         return torch.optim.Adam(self.parameters(), lr=self.learning_rate)
 
-    def create_coarse_graph(self, cluster_labels, original_edge_index):
-        unique_labels = np.unique(labels)
+    def create_coarse_graph(self, cluster_labels, original_edge_index, x):
+        unique_labels = np.unique(cluster_labels)
         num_clusters = len(unique_labels)
         coarse_grained_reps = []
 
         for label in unique_labels:
-            cluster_indices = np.where(labels == label)[0]
+            cluster_indices = np.where(cluster_labels == label)[0]
             cluster_x = x[cluster_indices]
             cluster_mean = torch.mean(cluster_x, dim=0)
             coarse_grained_reps.append(cluster_mean)
 
-        return torch.stack(coarse_grained_reps)
+        return torch.stack(coarse_grained_reps), original_edge_index
 
-    def create_coarse_graph(self, cluster_labels, original_edge_index):
-        num_clusters = cluster_labels.max() + 1
-        cluster_edges = set()
+    def coarse_grain_representation(self, x, labels):
+        num_clusters = torch.max(labels) + 1
+        coarse_grained_reps = []
 
-        for i, j in original_edge_index.t().tolist():
-            cluster_i = cluster_labels[i].item()
-            cluster_j = cluster_labels[j].item()
+        for i in range(num_clusters):
+            cluster_indices = torch.where(labels == i)[0]
+            cluster_x = x[cluster_indices]
+            cluster_mean = torch.mean(cluster_x, dim=0)
+            coarse_grained_reps.append(cluster_mean)
 
-            if cluster_i != cluster_j:
-                cluster_edges.add(tuple(sorted((cluster_i, cluster_j))))
-
-        coarse_edge_index = torch.tensor(list(cluster_edges), dtype=torch.long).t().contiguous()
-
-        return coarse_edge_index
+        return torch.stack(coarse_grained_reps, dim=0)
 
     def hierarchical_clustering(self, x, edge_index, num_layers, coarse_grain_dims):
-        cluster_labels = np.zeros((x.shape[0], num_layers))
+        self.labels = np.zeros((x.shape[0], num_layers))
         edge_indices = []
 
         for level in range(num_layers):
@@ -145,23 +142,24 @@ class VariationalAutoencoder(pl.LightningModule):
                 clustering = AgglomerativeClustering(n_clusters=coarse_grain_dims[level])
 
             clustering.fit(x)
-            labels = clustering.labels_
-            cluster_labels[:, level] = labels
+            self.labels[:, level] = clustering.labels_
 
-            coarse_edge_index = self.create_coarse_graph(torch.tensor(labels, dtype=torch.long), edge_index)
-            edge_indices.append(coarse_edge_index)
+            coarse_edge_index, edge_index = self.create_coarse_graph(torch.tensor(self.labels[:, level], dtype=torch.long), edge_index, x)
+            edge_indices.append(coarse_edge_index.unsqueeze(0).t())  # Reshape edge_index
 
-            x = self.coarse_grain_representation(x, labels)
+            x = self.coarse_grain_representation(x, self.labels[:, level])
 
-        return cluster_labels, edge_indices
+        return self.labels, edge_indices
 
 
 def estimate_latent_dim(data_list, variance_threshold=0.95):
-    node_features = torch.cat([data.x for data in data_list], dim=0).detach().numpy()
+    node_features = torch.cat([data.x for data in data_list], dim=0)
     pca = PCA()
     pca.fit(node_features)
-    cumulative_explained_variance = np.cumsum(pca.explained_variance_ratio_)
-    latent_dim = np.argmax(cumulative_explained_variance > variance_threshold)
+    explained_variance_ratio = torch.tensor(pca.explained_variance_ratio_, dtype=torch.float32)
+    cumulative_explained_variance = torch.cumsum(explained_variance_ratio, dim=0)
+    variance_threshold_tensor = torch.tensor(variance_threshold, dtype=torch.float32)
+    latent_dim = torch.nonzero(cumulative_explained_variance > variance_threshold_tensor.item()).min().item()
     return latent_dim
 
 
@@ -184,7 +182,7 @@ learning_rate = 0.001
 num_folds = 5
 translate_range = 0.1
 
-input_files_directory = 'C://Users//gemma//PycharmProjects//pythonProject1//autoencoder//pdb_files//graph_data'
+input_files_directory = 'C://Users//gemma//PycharmProjects//pythonProject1//autoencoder//pdb_files//graph_data_test'
 data_list = []
 for filename in os.listdir(input_files_directory):
     if filename.endswith(".pt"):
@@ -207,7 +205,6 @@ val_loader = DataLoader(val_data, batch_size=batch_size)
 
 model = VariationalAutoencoder(input_dim, latent_dim, num_layers, num_levels, coarse_grain_dims, dropout_rate)
 
-model.reconstruction_loss_function = nn.MSELoss()
 model.learning_rate = learning_rate
 
 trainer = pl.Trainer(max_epochs=num_epochs)
@@ -235,8 +232,8 @@ for data in new_data_loader:
     outputs, mu, logvar = model(x, edge_index)
 
     # Coarse-grained representations at different levels
-    x_level = model.encoder[0](outputs[0], edge_index)
-    labels = model.hierarchical_clustering(x_level, num_layers, coarse_grain_dims)
+    x_level = model.encoder[0](x, edge_index)
+    labels, edge_indices = model.hierarchical_clustering(x_level, edge_index, num_levels, coarse_grain_dims)
     coarse_grained_reps = []
     for level in range(num_levels):
         level_labels = labels[:, level]
@@ -245,7 +242,7 @@ for data in new_data_loader:
     new_coarse_grained_reps.append(coarse_grained_reps)
 
     # Reconstructed representations
-    reconstructed_reps = model.decoder[-1](outputs[-1], edge_index)
+    reconstructed_reps = model.decoder[-1](outputs, edge_index)
     new_reconstructed_reps.append(reconstructed_reps)
 
 # Save the coarse-grained representations

autoencoder/convert_to_graph.py
@@ -28,6 +28,10 @@ def pdb_to_torch_geometric(pdb_file, output_directory, distance_threshold=5.0):
     x = torch.tensor(np.array(coordinates), dtype=torch.float)  # Use atom coordinates as features
     edge_index = torch.tensor(edge_index, dtype=torch.long).t().contiguous()  # Transform to PyTorch format
 
+    # Ensure edge_index is two-dimensional
+    if edge_index.dim() == 1:
+        edge_index = edge_index.unsqueeze(0)
+
     data = Data(x=x, edge_index=edge_index)
 
     # Save the graph data
@@ -38,10 +42,10 @@ def pdb_to_torch_geometric(pdb_file, output_directory, distance_threshold=5.0):
 
 
 # Specify the path to your directory with pdb files
-pdb_directory = 'C://Users//gemma//PycharmProjects//pythonProject1//autoencoder//pdb_files///chig'
+pdb_directory = 'C://Users//gemma//PycharmProjects//pythonProject1//autoencoder//pdb_files//pdb_files_1'
 
 # Specify the path to your output directory
-output_directory = 'C://Users//gemma//PycharmProjects//pythonProject1//autoencoder//pdb_files//chi_graph'
+output_directory = 'C://Users//gemma//PycharmProjects//pythonProject1//autoencoder//pdb_files//graph_data_test'
 
 # Get all pdb files in the directory
 pdb_files = glob.glob(os.path.join(pdb_directory, '*.pdb'))

autoencoder/view_graph.py
@@ -1,26 +1,43 @@
 import networkx as nx
 import matplotlib.pyplot as plt
 import torch
-
+import os
+from sklearn.decomposition import PCA
 
 def visualize(data):
     G = nx.Graph()  # Create an empty NetworkX graph
     edge_index = data.edge_index.numpy()
+    coordinates = data.x.numpy()  # Get the atom coordinates
 
-    # Add edges to the graph
+    # Use PCA to reduce the dimension from 3 to 2
+    pca = PCA(n_components=2)
+    coordinates_2d = pca.fit_transform(coordinates)
+
+    # Add nodes and edges to the graph
+    for i in range(coordinates_2d.shape[0]):
+        G.add_node(i, pos=coordinates_2d[i])
+
     for i in range(edge_index.shape[1]):
         G.add_edge(edge_index[0, i], edge_index[1, i])
 
-    # Use the spring layout to position nodes
-    pos = nx.spring_layout(G, seed=42)
+    # Use the coordinates for positions
+    pos = nx.get_node_attributes(G, 'pos')
 
     # Draw the graph
     nx.draw(G, pos, with_labels=True)
     plt.show()
 
+# Define the file path
+file_path = 'C://Users//gemma//PycharmProjects//pythonProject1//autoencoder//pdb_files//graph_data_test//1X7B_2.pdb.pt'
 
-# Load your graph
-data = torch.load('C://Users//gemma//PycharmProjects//pythonProject1//autoencoder//new_protein_coarse_grained_reps.pt')
+# Check if the file exists
+if os.path.exists(file_path):
+    # Load your graph
+    data = torch.load(file_path)
 
-# Visualize it
-visualize(data)
+    # Visualize it
+    visualize(data)
+else:
+    print(f"The file {file_path} does not exist.")
+
+