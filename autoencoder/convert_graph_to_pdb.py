3a1174c236c8ad985c46e410a39d3c5ae5b52102
Gemma <gwhite11@student.bbk.ac.uk>
2023-07-12 22:28:05 +0100

    Maybe we need to create bead identities

--- autoencoder/convert_graph_to_pdb.py
+++ autoencoder/convert_graph_to_pdb.py
@@ -18,28 +18,32 @@ def rebuild_molecule(original_pdb_file, coarse_grained_pdb_file, output_pdb_file
             'atom_type': atom.get_id(),
             'residue_name': atom.get_parent().get_resname(),
             'residue_id': atom.get_parent().get_id(),
-            'coord': atom.coord
+            'coord': atom.coord,
+            'bead_id': None  # Initialize bead identity
         })
 
-    # Extract coordinates from the coarse-grained PDB file
-    coarse_grained_coords = []
+    # Extract bead information from the coarse-grained PDB file
+    bead_info = {}
     for atom in coarse_grained_structure.get_atoms():
         if isinstance(atom.coord, torch.Tensor):  # Skip non-coordinate nodes
-            coarse_grained_coords.append(atom.coord.numpy())
+            bead_id = atom.bead_id
+            if bead_id not in bead_info:
+                bead_info[bead_id] = []
+            bead_info[bead_id].append(atom)
 
-    # Check if the number of atoms matches the number of coarse-grained nodes
-    if len(original_atoms) != len(coarse_grained_coords):
-        raise ValueError("Mismatch in the number of atoms and coarse-grained nodes.")
-
-    # Update the original atom coordinates with the coarse-grained coordinates
-    for i, atom in enumerate(original_atoms):
-        atom['coord'] = coarse_grained_coords[i]
+    # Assign bead identities to the corresponding atoms in the original PDB structure
+    for bead_id, atoms in bead_info.items():
+        for atom in atoms:
+            atom_index = atom.get_serial_number()
+            if atom_index < len(original_atoms):
+                original_atoms[atom_index]['bead_id'] = bead_id
 
     # Write the updated atom information to a new PDB file for the coarse-grained version
     with open(output_pdb_file, 'w') as f:
         for atom in original_atoms:
             atom_line = f"ATOM  {atom['atom_type']:4s} {atom['atom_name']:4s} {atom['residue_name']:3s} " \
-                        f" {atom['residue_id']:4s}    {atom['coord'][0]:8.3f}{atom['coord'][1]:8.3f}{atom['coord'][2]:8.3f}\n"
+                        f" {atom['residue_id']:4s}    {atom['coord'][0]:8.3f}{atom['coord'][1]:8.3f}{atom['coord'][2]:8.3f}" \
+                        f" {atom['bead_id']:4s}\n"  # Include bead identity in the PDB line
             f.write(atom_line)
 
 
