93739a9cf9cfe9d481c783b17cf37c0afcf63779
Gemma <gwhite11@student.bbk.ac.uk>
2023-08-10 23:25:18 +0100

    hierarchical clustering - makes a dendrogram of clusters and asks where to cut it

---
+++ autoencoder/graph_with_energy.py
@@ -0,0 +1,142 @@
+import numpy as np
+import torch
+from torch_geometric.data import Data
+import os
+import glob
+import networkx as nx
+import pandas as pd
+
+
+def distance(a, b):
+    return np.linalg.norm(a - b)
+
+
+def calculate_angle(coords_a, coords_b, coords_c):
+    vector_ab = coords_b - coords_a
+    vector_bc = coords_b - coords_c
+    cosine_angle = np.dot(vector_ab, vector_bc) / (np.linalg.norm(vector_ab) * np.linalg.norm(vector_bc))
+    angle_rad = np.arccos(cosine_angle)
+    return np.degrees(angle_rad)
+
+
+# Load the CSV file into a DataFrame
+csv_file_path = 'C://Users//gemma//PycharmProjects//pythonProject1//combined_output.csv'
+df = pd.read_csv(csv_file_path)
+
+
+def lookup_features(residue, atom_name):
+    # Search for the matching row in the dataframe
+    row = df[(df['Residue'] == residue) & (df['Atom Name'] == atom_name)]
+    if not row.empty:
+        atom_charge = row['Atom Charge'].values[0]
+        epsilon = row['Epsilon'].values[0]
+        sigma = row['Sigma'].values[0]
+        return atom_charge, epsilon, sigma
+    else:
+        # Return default values if not found
+        return 0.0, 0.0, 0.0
+
+
+def pdb_to_pyg_data(pdb_file, output_directory):
+    # Initialize graph
+    G = nx.Graph()
+
+    # Atom types and residue types
+    atom_types = []
+    residue_types = []
+    node_idx_map = {}  # Map from PDB atom index to consecutive node index
+
+    # Read molecule from PDB file
+    with open(pdb_file, 'r') as f:
+        for line in f:
+            if line.startswith('ATOM'):
+                atom_idx = int(line[6:11].strip())
+                atom_symbol = line[12:16].strip()
+                atom_coords = np.array([float(line[30:38]), float(line[38:46]), float(line[46:54])])
+                residue_name = line[17:20].strip()
+                residue_id = int(line[22:26].strip())
+
+                node_idx = len(G.nodes)
+                node_idx_map[atom_idx] = node_idx  # Store the mapping
+
+                G.add_node(node_idx, symbol=atom_symbol, coords=atom_coords, residue=residue_name)
+
+                atom_types.append(atom_symbol)
+                residue_types.append(residue_name)
+
+    # Add bonds to graph using the node index mapping
+    with open(pdb_file, 'r') as f:
+        prev_atom_idx = None
+        for line in f:
+            if line.startswith('ATOM'):
+                atom_idx = int(line[6:11].strip())
+                if prev_atom_idx is not None:
+                    start_atom_idx = node_idx_map[prev_atom_idx]
+                    end_atom_idx = node_idx_map[atom_idx]
+                    bond_type = "unknown"
+                    bond_distance = distance(G.nodes[start_atom_idx]['coords'], G.nodes[end_atom_idx]['coords'])
+                    G.add_edge(start_atom_idx, end_atom_idx, bond_type=bond_type, distance=bond_distance)
+                prev_atom_idx = atom_idx
+
+    # Getting atom charge, epsilon, and sigma for each atom
+    features = []
+    for residue, atom in zip(residue_types, atom_types):
+        atom_charge, epsilon, sigma = lookup_features(residue, atom)
+        features.append([atom_charge, epsilon, sigma])
+
+    features = np.array(features)
+    coords = np.array([G.nodes[i]['coords'] for i in G.nodes])
+    node_features = np.hstack((coords, features))
+    node_features = torch.tensor(node_features, dtype=torch.float)
+
+    # Edge indices
+    edge_index = torch.tensor(list(G.edges)).long().t()
+
+    # Calculate angles between connected atoms and include them as additional edge features
+    for edge in G.edges:
+        start_atom_idx, middle_atom_idx, end_atom_idx = edge[0], edge[1], None
+        for neighbor in G.neighbors(middle_atom_idx):
+            if neighbor != start_atom_idx:
+                end_atom_idx = neighbor
+                break
+
+        if end_atom_idx is not None:
+            start_coords = G.nodes[start_atom_idx]['coords']
+            middle_coords = G.nodes[middle_atom_idx]['coords']
+            end_coords = G.nodes[end_atom_idx]['coords']
+            angle = calculate_angle(start_coords, middle_coords, end_coords)
+            G.edges[edge]['angle'] = angle
+
+    # Create PyTorch Geometric Data object with additional edge features (angles)
+    edge_attr = [G.edges[edge].get('angle', 0.0) for edge in G.edges]
+    edge_attr = torch.tensor(edge_attr, dtype=torch.float).view(-1, 1)
+
+    data = Data(x=node_features, edge_index=edge_index, edge_attr=edge_attr)
+
+    # Save the graph data
+    output_file = os.path.join(output_directory, os.path.basename(pdb_file) + '.pt')
+    torch.save(data, output_file)
+
+    return data
+
+
+# Specify the path to your directory with pdb files
+pdb_directory = 'C://Users//gemma//PycharmProjects//pythonProject1//autoencoder//pdb_files//pdb_files_6'
+
+# Specify the path to your output directory
+output_directory = 'C://Users//gemma//PycharmProjects//pythonProject1//autoencoder//pdb_files//graphs_2'
+
+# Get all pdb files in the directory
+pdb_files = glob.glob(os.path.join(pdb_directory, '*.pdb'))
+
+# Iterate over all pdb files and create corresponding graphs
+print(f"Found {len(pdb_files)} PDB files.")
+
+for pdb_file in pdb_files:
+    try:
+        print(f"Processing {pdb_file}...")
+        pdb_to_pyg_data(pdb_file, output_directory=output_directory)
+        print(f"Finished processing {pdb_file}.")
+    except RuntimeError as e:
+        print(f"Error processing {pdb_file}: {str(e)}")
+        continue
---
+++ autoencoder/vae_pooling_8.py
@@ -0,0 +1,367 @@
+import os
+import torch
+import torch.nn.functional as F
+from torch.utils.data import Dataset, DataLoader, random_split
+from torch_geometric.data import Batch
+from torch_geometric.nn import SAGEConv
+from torch.nn import Linear, Dropout, BatchNorm1d
+from scipy.spatial.distance import cdist
+import networkx as nx
+import matplotlib.pyplot as plt
+from torch_geometric.utils import to_networkx
+from Bio.PDB import PDBIO, Chain, Residue, Atom, Model, Structure
+import numpy as np
+from Bio.PDB import PDBParser
+from collections import Counter
+import csv
+from sklearn.cluster import AgglomerativeClustering
+from scipy.cluster.hierarchy import dendrogram, linkage
+
+class VAE(torch.nn.Module):
+    def __init__(self, in_channels, hidden_channels, out_channels):
+        super(VAE, self).__init__()
+        self.dropout_prob = 0.5  # Adjust the dropout probability if needed
+
+        # Encoder layers
+        self.encoder = torch.nn.ModuleList([
+            SAGEConv(in_channels, hidden_channels),
+            Dropout(self.dropout_prob),
+            BatchNorm1d(hidden_channels),
+            SAGEConv(hidden_channels, 2 * hidden_channels),
+            Dropout(self.dropout_prob),
+            BatchNorm1d(2 * hidden_channels),
+            SAGEConv(2 * hidden_channels, 2 * out_channels)  # latent space with mean and log_std
+        ])
+
+        # Decoder layers
+        self.decoder = torch.nn.ModuleList([
+            Linear(out_channels, 2 * hidden_channels),
+            Dropout(self.dropout_prob),
+            BatchNorm1d(2 * hidden_channels),
+            Linear(2 * hidden_channels, hidden_channels),
+            Dropout(self.dropout_prob),
+            BatchNorm1d(hidden_channels),
+            Linear(hidden_channels, in_channels)
+        ])
+        self.out_channels = out_channels
+
+    def encode(self, x, edge_index):
+        x = F.relu(self.encoder[0](x, edge_index))
+        x = self.encoder[1](x)  # Dropout
+        x = self.encoder[2](x)  # BatchNorm
+        x = F.relu(self.encoder[3](x, edge_index))
+        x = self.encoder[4](x)  # Dropout
+        x = self.encoder[5](x)  # BatchNorm
+        x = self.encoder[6](x, edge_index)
+        mean, log_std = x.chunk(2, dim=-1)  # Split into mean and log_std
+        return mean, log_std
+
+    def decode(self, z):
+        h = F.relu(self.decoder[0](z))
+        h = self.decoder[1](h)  # Dropout
+        h = self.decoder[2](h)  # BatchNorm
+        h = F.relu(self.decoder[3](h))
+        h = self.decoder[4](h)  # Dropout
+        h = self.decoder[5](h)  # BatchNorm
+        return self.decoder[6](h)
+
+    def reparameterize(self, mean, log_std):
+        std = log_std.exp()
+        epsilon = torch.randn_like(std)
+        z = mean + std * epsilon
+        return z
+
+    def forward(self, x, edge_index):
+        mean, log_std = self.encode(x, edge_index)
+        z = self.reparameterize(mean, log_std)
+
+        # Apply global mean pooling on the latent space
+        z = self.global_mean_pool(z)
+
+        return z
+
+    def global_mean_pool(self, z):
+        return torch.mean(z, dim=0)
+
+    def recon_loss(self, x, mean, log_std, edge_index):
+        z = self.reparameterize(mean, log_std)
+        x_recon = self.decode(z)
+        recon_loss = F.mse_loss(x_recon, x, reduction='mean')
+        return recon_loss
+
+    def kl_divergence(self, mean, log_std):
+        std = log_std.exp()
+        kl_loss = -0.5 * torch.sum(1 + 2 * log_std - mean.pow(2) - std.pow(2), dim=-1)
+        return kl_loss.mean()
+
+
+def train_vae(model, loader, optimizer):
+    model.train()
+
+    total_loss = 0
+    for data in loader:
+        optimizer.zero_grad()
+        mean, log_std = model.encode(data.x, data.edge_index)
+        recon_loss = model.recon_loss(data.x, mean, log_std, data.edge_index)
+        kl_loss = model.kl_divergence(mean, log_std)
+        loss = recon_loss + kl_loss
+        loss.backward()
+        optimizer.step()
+        total_loss += loss.item()
+
+    return total_loss / len(loader)
+
+
+class CustomGraphDataset(Dataset):
+    def __init__(self, data_folder, numerical_indices):
+        self.data_file_list = [os.path.join(data_folder, filename) for filename in os.listdir(data_folder) if
+                               filename.endswith('.pt')]
+        self.numerical_indices = numerical_indices
+
+        # Compute mean and std on the training set
+        self.mean, self.std = self._compute_mean_std()
+
+    def __len__(self):
+        return len(self.data_file_list)
+
+    def __getitem__(self, index):
+        graph_data = torch.load(self.data_file_list[index])
+
+        # Normalize only numerical features
+        graph_data.x[:, self.numerical_indices] = (graph_data.x[:, self.numerical_indices] - self.mean) / self.std
+
+        return graph_data
+
+    def _compute_mean_std(self):
+        all_data = [torch.load(file) for file in self.data_file_list]
+        all_features = torch.cat([data.x[:, self.numerical_indices] for data in all_data], dim=0)
+        return torch.mean(all_features, dim=0), torch.std(all_features, dim=0)
+
+
+def collate_fn(batch):
+    return Batch.from_data_list(batch)
+
+
+def visualize_original_and_reconstructed(original_data, reconstructed_data):
+    G_original = to_networkx(original_data)
+    G_reconstructed = to_networkx(reconstructed_data)
+
+    if original_data.is_directed():
+        G_original = G_original.to_directed()
+
+    if reconstructed_data.is_directed():
+        G_reconstructed = G_reconstructed.to_directed()
+
+    plt.figure(figsize=(12, 6))
+
+    # Visualize the original graph on the left
+    plt.subplot(1, 2, 1)
+    pos_original = nx.spring_layout(G_original, seed=42)  # You can change the layout algorithm if needed
+    nx.draw(G_original, pos_original, with_labels=True)
+    plt.title("Original Graph")
+
+    # Visualize the reconstructed graph on the right
+    plt.subplot(1, 2, 2)
+    pos_reconstructed = nx.spring_layout(G_reconstructed, seed=42)  # You can change the layout algorithm if needed
+    nx.draw(G_reconstructed, pos_reconstructed, with_labels=True)
+    plt.title("Reconstructed Graph")
+
+    plt.tight_layout()
+    plt.show()
+
+
+if __name__ == '__main__':
+    data_folder = "C://Users//gemma//PycharmProjects//pythonProject1//autoencoder//pdb_files//graphs_2"
+    numerical_indicies = [0, 1, 2, 3, 4, 5]
+    dataset = CustomGraphDataset(data_folder, numerical_indicies)
+
+    # Define the split size
+    train_size = int(0.8 * len(dataset))  # Use 80% of the data for training
+    valid_size = len(dataset) - train_size  # Use the rest for validation
+
+    # Perform the split
+    train_dataset, valid_dataset = random_split(dataset, [train_size, valid_size])
+
+    batch_size = 4  # Adjust the batch size if memory crashes
+
+    # Create data loaders for training and validation sets
+    train_loader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True, drop_last=True, num_workers=0,
+                              collate_fn=collate_fn)
+    valid_loader = DataLoader(valid_dataset, batch_size=batch_size, shuffle=False, drop_last=True, num_workers=0,
+                              collate_fn=collate_fn)
+
+    # Define the model
+    in_channels = train_dataset[0].num_node_features
+    hidden_channels = 128
+    out_channels = 28
+    model = VAE(in_channels, hidden_channels, out_channels)
+    num_epochs = 100
+
+    # Set up the optimizer
+    optimizer = torch.optim.Adam(model.parameters(), lr=0.001)
+    #
+    # # Training loop
+    # for epoch in range(num_epochs):
+    #     loss = train_vae(model, train_loader, optimizer)
+    #     if epoch % 10 == 0:
+    #         print(f"Epoch: {epoch}, Loss: {loss}")
+    #
+    # # Save the model parameters
+    # torch.save(model.state_dict(), 'model_new_4.pth')
+
+    # Define the model
+    model = VAE(in_channels, hidden_channels, out_channels)
+
+    # Load the model parameters
+    model.load_state_dict(torch.load('model_new_4.pth'))
+
+    new_graph = "C://Users//gemma//PycharmProjects//pythonProject1//autoencoder//pdb_files//test_graph_1//7E29_20.pdb.pt"
+    original_pdb = 'C://Users//gemma//PycharmProjects//pythonProject1//autoencoder//pdb_files//test_pdb_1//7E29_20.pdb'
+
+    # Compute embeddings for all nodes in the new graph
+    model.eval()
+    with torch.no_grad():
+        # new_data is your new graph data
+        new_graph_data = torch.load(new_graph)
+        new_graph_data.x[:, numerical_indicies] = (new_graph_data.x[:, numerical_indicies] - dataset.mean) / dataset.std
+        mean, log_std = model.encode(new_graph_data.x, new_graph_data.edge_index)
+        new_embeddings = model.reparameterize(mean, log_std).cpu().numpy()
+
+        # Reconstruct the graph using the decoder
+        reconstructed_graph_data = new_graph_data.clone()
+        reconstructed_graph_data.x = model.decode(torch.tensor(new_embeddings))
+
+    # Compute the linkage matrix
+    linked = linkage(new_embeddings, 'ward')  # 'ward' is one method, you can explore 'single', 'complete', etc.
+
+    # Plot the dendrogram to visualize the structure
+    plt.figure(figsize=(17, 10))
+    dendrogram(linked)
+    plt.title('Dendrogram')
+    plt.ylabel('Euclidean distances')
+    plt.show()
+
+    # Decide on a height to cut the dendrogram
+    cut_height = float(input("Enter the height at which to cut the dendrogram to form clusters: "))
+
+    # Perform hierarchical Clustering
+    cluster = AgglomerativeClustering(n_clusters=None, distance_threshold=cut_height, linkage='ward')
+    labels = cluster.fit_predict(new_embeddings)
+
+
+    def visualize_clusters(data, labels, original_pdb_path):
+        # Parse the original PDB file
+        parser = PDBParser()
+        structure = parser.get_structure("original", original_pdb_path)
+
+        # Extract the coordinates and identities of each atom
+        atom_info = {}
+        for model in structure:
+            for chain in model:
+                for residue in chain:
+                    for atom in residue:
+                        atom_info[atom.serial_number] = {'coord': atom.coord.tolist(),
+                                                         'residue_name': residue.resname,
+                                                         'atom_name': atom.name}
+
+        # Convert the PyTorch Geometric graph data to a NetworkX graph
+        G = to_networkx(data)
+
+        # Check if the graph is directed
+        if data.is_directed():
+            G = G.to_directed()
+
+        # Create a color map from cluster labels
+        cmap = [labels[node] for node in G]
+
+        plt.figure(figsize=(8, 8))
+        nx.draw(G, node_color=cmap, with_labels=True, cmap=plt.cm.tab10)
+        plt.show()
+
+        # Save the coarse grained graph to a PDB file
+        coarse_grained_graph = Structure.Structure("H")
+        model = Model.Model(0)  # add a model with model_id = 0
+        chain = Chain.Chain("A")
+        model.add(chain)
+        coarse_grained_graph.add(model)
+
+        # Create clusters of atoms (or 'beads')
+        clusters = {}
+        for node, label in enumerate(labels):
+            if label in clusters:
+                clusters[label].append(node)
+            else:
+                clusters[label] = [node]
+
+        # For writing to CSV
+        with open('outputs/beads_to_atoms_5.csv', 'w', newline='') as csvfile:
+            beadwriter = csv.writer(csvfile)
+            beadwriter.writerow(["BeadID", "ImportantAtom", "AllAtomsInCluster"])
+
+            # Assign atom coordinates in each cluster by their centroid
+            for label, nodes in clusters.items():
+                # Use the coordinates from the original PDB file
+                centroid_atoms = []
+                try:
+                    centroid = np.mean([atom_info[node + 1]['coord'] for node in nodes], axis=0)
+                    for node in nodes:
+                        centroid_atoms.append(
+                            f"{atom_info[node + 1]['atom_name']} ({atom_info[node + 1]['residue_name']})")
+                except KeyError as e:
+                    print(f'KeyError: {e} not found in atom_info')
+
+                # Determine the most common atom and residue type for the atoms in the cluster
+                atom_types = [atom_info[node + 1]['atom_name'] for node in nodes]
+                residue_types = [atom_info[node + 1]['residue_name'] for node in nodes]
+
+                most_common_atom_type = Counter(atom_types).most_common(1)[0][0]
+                most_common_residue_type = Counter(residue_types).most_common(1)[0][0]
+                most_common_element = most_common_atom_type[
+                    0]  # Assumes the first character of atom name is the element type
+
+                # Write to CSV
+                beadwriter.writerow([f"B{label:03d}", most_common_atom_type, "; ".join(centroid_atoms)])
+
+                # Continue with the PDB writing
+                residue = Residue.Residue((" ", label, " "), most_common_residue_type, " ")
+                atom = Atom.Atom(most_common_atom_type, centroid.tolist(), 1, 0, " ",
+                                 most_common_atom_type, label, most_common_element)
+                residue.add(atom)
+                chain.add(residue)
+
+            io = PDBIO()
+            io.set_structure(coarse_grained_graph)
+            io.save("coarse_grained_graph.pdb_15")
+
+
+    visualize_clusters(new_graph_data, labels, original_pdb)
+
+    # Convert the PyTorch Geometric graph data to a NetworkX graph
+    G = to_networkx(new_graph_data)
+
+    # Check if the graph is directed
+    if new_graph_data.is_directed():
+        G = G.to_directed()
+
+    # Step 1: Group nodes based on cluster labels
+    clusters = {}
+    for node, label in zip(G.nodes(), labels):
+        if label not in clusters:
+            clusters[label] = [node]
+        else:
+            clusters[label].append(node)
+
+    # Step 2: Plot all nodes in the same plot with different colors for each cluster
+    plt.figure(figsize=(8, 8))
+    pos = nx.spring_layout(G)  # Adjust the layout algorithm if needed
+
+    for i, cluster_nodes in enumerate(clusters.values()):
+        nx.draw_networkx_nodes(G, pos, nodelist=cluster_nodes, node_color='C{}'.format(i), cmap=plt.cm.tab10,
+                               node_size=200)
+
+    nx.draw_networkx_edges(G, pos)
+    plt.title(f"Clusters")
+    plt.show()
+
+    # Visualize the original and reconstructed graphs
+    visualize_original_and_reconstructed(new_graph_data, reconstructed_graph_data)
